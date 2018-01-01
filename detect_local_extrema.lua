57c3a2edaab8b6a826faa885c857b9167c5d7e85
Sokomine <wegwerf@anarres.dyndns.org>
2018-01-01 22:43:59 +0100

    used more local variables

--- detect_local_extrema.lua
+++ detect_local_extrema.lua
@@ -11,11 +11,12 @@ find_flat_land.detect_local_extrema = function( heightmap, minp, maxp )
 	-- egment_id in min_or_max will be revoked
 	local update_incl_merged = function( segment_id, merged, min_or_max )
 		local m;
-		for m,_ in pairs( merged[ segment_id ] or {}) do
-			min_or_max[ m ] = false;
+		if( merged[ segment_id ]) then
+			for m,_ in pairs( merged[ segment_id ] or {}) do
+				min_or_max[ m ] = false;
+			end
 		end
 		min_or_max[ segment_id ] = nil;
-		return min_or_max;
 	end
 
 
@@ -26,6 +27,10 @@ find_flat_land.detect_local_extrema = function( heightmap, minp, maxp )
 	-- only merges of candidates for local maximum and minimum are stored
 	-- (the rest of the segments is of not much intrest)
 	local merged = {};
+
+	-- into which other segment has this one been merged?
+	-- merged_into_other[ a ] = b means: a has been merged into b
+	local merged_into_other = {};
 
 	-- each segment needs a uniq number
 	local segment_nr = 0;
@@ -86,74 +91,70 @@ find_flat_land.detect_local_extrema = function( heightmap, minp, maxp )
 			end
 		end
 
+		local this_segment = segment[ i ];
+
 		-- if this node is LOWER than the one at (x-1,z), then...
 		-- (or if it is at the border of the mapchunk)
-		if( segment[ i-1 ] or ax==maxp.x) then
+		if( last_col_segment or ax==maxp.x) then
 			if(     height < lastheight) then
 				-- the segment containing the node at (x-1,z) cannot be a local minimum
-				update_incl_merged( segment[ i-1], merged, segment_is_local_minimum );
+				update_incl_merged( last_col_segment, merged, segment_is_local_minimum );
 				-- this segment cannot be a local maximum
-				update_incl_merged( segment[ i  ], merged, segment_is_local_maximum );
+				update_incl_merged( this_segment,     merged, segment_is_local_maximum );
 			-- if this node is HIGHER than the one at (x-1,z), then...
 			elseif( height > lastheight ) then
 				-- the segment containing the node at (x-1,z) cannot be a local maximum
-				update_incl_merged( segment[ i-1], merged, segment_is_local_maximum );
+				update_incl_merged( last_col_segment, merged, segment_is_local_maximum );
 				-- this segment cannot be a local minimum
-				update_incl_merged( segment[ i  ], merged, segment_is_local_minimum );
+				update_incl_merged( this_segment,     merged, segment_is_local_minimum );
 			end
 		end
 
 		-- do the same for the node at (x,z-1)
-		if( segment[ i-chunksize ] or az==maxp.z) then
+		if( last_row_segment or az==maxp.z) then
 			if(     height < lastrowheight) then
-				update_incl_merged( segment[ i-chunksize ], merged, segment_is_local_minimum );
-				update_incl_merged( segment[ i           ], merged, segment_is_local_maximum );
+				update_incl_merged( last_row_segment, merged, segment_is_local_minimum );
+				update_incl_merged( this_segment,     merged, segment_is_local_maximum );
 			elseif( height > lastrowheight) then
-				update_incl_merged( segment[ i-chunksize ], merged, segment_is_local_maximum );
-				update_incl_merged( segment[ i           ], merged, segment_is_local_minimum );
+				update_incl_merged( last_row_segment, merged, segment_is_local_maximum );
+				update_incl_merged( this_segment,     merged, segment_is_local_minimum );
 
 			-- special case: if (x,z-1) has the same height but is part of
 			-- a diffrent segment, then the segments need to be merged
 			elseif( height == lastrowheight
-			    and segment[ i ] ~= segment[ i-chunksize ]) then
+			    and this_segment ~= last_row_segment
+			    and ( not( merged[ this_segment ])
+			       or not( merged[ this_segment ][ last_row_segment ]))) then
+
+				-- remember the merge
+				if( not( merged[ this_segment ])) then
+					merged[ this_segment ] = {};
+				end
+				merged[ this_segment ][ last_row_segment ] = true;
+				for m,_ in pairs( merged[ last_row_segment ] or {}) do
+					merged[ this_segment ][ m ] = true;
+					merged_into_other[ m ] = this_segment;
+				end
+				merged_into_other[ last_row_segment ] = this_segment;
+
 				-- if the to-be-merged segment has already been determined *not* to
 				-- be a local maximum or minimum, then this segment likewise cannot
 				-- be such a minimum or maximum
-				if(     not( segment_is_local_maximum[ segment[ i-chunksize ] ])) then
-					update_incl_merged( segment[ i ], merged, segment_is_local_maximum );
+				if(     not( segment_is_local_maximum[ last_row_segment ])) then
+					update_incl_merged( this_segment, merged, segment_is_local_maximum );
 				-- if the segment at (x,z-1) believed it was a maximum, but this segment
 				-- here is to be merged with it and is *not* a maximum, then we need
 				-- to propagate this update down to all segments the one at (x,z-1)
 				-- had been merged with
-				elseif( not( segment_is_local_maximum[ segment[ i ] ])) then
-					update_incl_merged( segment[ i-chunksize ], merged, segment_is_local_maximum );
-				elseif(      segment_is_local_maximum[ segment[ i-chunksize ] ]
-				         and segment_is_local_maximum[ segment[ i ] ]) then
-					-- remember the merge
-					if( not( merged[ segment[ i ] ])) then
-						merged[ segment[ i ] ] = {};
-					end
-					merged[ segment[ i ] ][ segment[ i-chunksize ] ] = true;
-					for m,_ in pairs( merged[ segment[ i-chunksize ] ] or {}) do
-						merged[ segment[ i ] ][ m ] = true;
-					end
+				elseif( not( segment_is_local_maximum[ this_segment ])) then
+					update_incl_merged( last_row_segment, merged, segment_is_local_maximum );
 				end
 
 				-- same for local minimum
-				if( not( segment_is_local_minimum[ segment[ i-chunksize ] ])) then
-					segment_is_local_minimum[ segment[ i ] ] = nil;
-				elseif( not( segment_is_local_minimum[ segment[ i ] ])) then
-					update_incl_merged( segment[ i-chunksize ], merged, segment_is_local_minimum );
-				elseif(      segment_is_local_minimum[ segment[ i-chunksize ] ]
-				         and segment_is_local_minimum[ segment[ i ] ]) then
-					-- remember the merge
-					if( not( merged[ segment[ i ] ])) then
-						merged[ segment[ i ] ] = {};
-					end
-					merged[ segment[ i ] ][ segment[ i-chunksize ] ] = true;
-					for m,_ in pairs( merged[ segment[ i-chunksize ] ] or {}) do
-						merged[ segment[ i ] ][ m ] = true;
-					end
+				if( not( segment_is_local_minimum[ last_row_segment ])) then
+					segment_is_local_minimum[ this_segment ] = nil;
+				elseif( not( segment_is_local_minimum[ this_segment ])) then
+					update_incl_merged( last_row_segment, merged, segment_is_local_minimum );
 				end
 			end
 		end
@@ -166,7 +167,8 @@ find_flat_land.detect_local_extrema = function( heightmap, minp, maxp )
 	end
 	end
 
-	return { segment = segment, maxima = segment_is_local_maximum, minima = segment_is_local_minimum };
+	return { segment = segment, maxima = segment_is_local_maximum, minima = segment_is_local_minimum,
+			merged = merged_into_other  };
 end
 
 
@@ -192,15 +194,25 @@ find_flat_land.visualize_extrema = function( heightmap, minp, maxp, orig_heightm
 		i = i+1;
 		local segment_id = data.segment[ i ];
 		local height = heightmap[i];
+--[[
+		-- color segments
+		if( data.merged[ segment_id ]) then
+			segment_id = data.merged[ segment_id ];
+		end
+		minetest.set_node( {x=ax, y=height, z=az}, {name="find_flat_land:segment", param2 = segment_id%256});
+--]]
+
 		if(     data.minima[ segment_id ]) then
 			-- do not really fill the bottoms of valleys - just illustrate it
-			if( orig_heightmap[ i ] >= height ) then
+			if( orig_heightmap[ i ] >= height and node_name_for_minimum) then
 				minetest.set_node( {x=ax, y=height, z=az}, {name=node_name_for_minimum, param2 = 0});
 			end
 			heightmap2[ i ] = height+1;
 
 		elseif( data.maxima[ segment_id ]) then
-			minetest.set_node( {x=ax, y=height, z=az}, {name=node_name_for_maximum, param2 = 0});
+			if( node_name_for_maximum ) then
+				minetest.set_node( {x=ax, y=height, z=az}, {name=node_name_for_maximum, param2 = 0});
+			end
 			heightmap2[ i ] = height-1;
 		end
 	end
--- init.lua
+++ init.lua
@@ -4,6 +4,16 @@ find_flat_land = {};
 dofile(minetest.get_modpath( minetest.get_current_modname() ).."/detect_flat_land.lua");
 dofile(minetest.get_modpath( minetest.get_current_modname() ).."/detect_local_extrema.lua");
 
+--- just a node that can be colored and thus visualize the created segments
+minetest.register_node("find_flat_land:segment", {
+	description = "Segment Indicator",
+	tiles = {"default_clay.png"},
+	is_ground_content = false,
+	groups = {oddly_breakable_by_hand=3,cracky=3,snappy=3,sappy=3},
+	paramtype2 = "color",
+	palette = "unifieddyes_palette_extended.png",
+	});
+
 minetest.register_on_generated(function(minp, maxp, seed)
 	-- the flat_area-detection works below sealevel as well
 	if( minp.y < -64 or minp.y > 500) then
@@ -18,19 +28,17 @@ minetest.register_on_generated(function(minp, maxp, seed)
 
 	local t_last = minetest.get_us_time();
 	local chunksize = maxp.x-minp.x+1;
-
-
 	local res = find_flat_land.detect_local_extrema( heightmap, minp, maxp );
 	-- turn bottom of valleys into yellow wool; turn mountain tops into orange wool
-	local heightmap2 = find_flat_land.visualize_extrema( heightmap, minp, maxp, heightmap, res, "wool:yellow","wool:orange");
+	local heightmap2 = find_flat_land.visualize_extrema( heightmap, minp, maxp, heightmap, res, nil, nil); --"wool:yellow","wool:orange");
 
 	res = find_flat_land.detect_local_extrema( heightmap2, minp, maxp );
 	-- turn layer one top of valley bottoms into mangeta wool; turn mountain layers one below top into cyan wool
-	heightmap2 = find_flat_land.visualize_extrema( heightmap2, minp, maxp, heightmap, res, "wool:magenta", "wool:cyan" );
+	heightmap2 = find_flat_land.visualize_extrema( heightmap2, minp, maxp, heightmap, res, nil, nil); --"wool:magenta", "wool:cyan" );
 
 	res = find_flat_land.detect_local_extrema( heightmap2, minp, maxp );
 	-- turn layer two from valley bottoms into pink wool; turn mountain layers two below top into blue wool
-	heightmap2 = find_flat_land.visualize_extrema( heightmap2, minp, maxp, heightmap, res, "wool:pink", "wool:blue" );
+	heightmap2 = find_flat_land.visualize_extrema( heightmap2, minp, maxp, heightmap, res, nil, nil); --"wool:pink", "wool:blue" );
 --[[
 	-- analyze how many subsequent blocks have the same height in each row and column
 	local same_height_count = find_flat_land.do_same_height_count( heightmap2, minp, maxp );
@@ -42,6 +50,6 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	for nr, i in pairs( buildplace_list ) do
 		minetest.set_node( {x=minp.x+(i%chunksize)-1, y=heightmap2[ i ], z=minp.z+math.floor(i/chunksize)}, {name="default:meselamp"});
 	end
-       	print( 'TIME ELAPSED: '..tostring( minetest.get_us_time() - t_last ) );
 --]]
+	print( 'TIME ELAPSED: '..tostring( minetest.get_us_time() - t_last ) );
 end);
